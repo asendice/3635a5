e1a4b2af5c509d96198a31bd6c37c777b32c06a5
Dylan Travis <dylan.travis23@outlook.com>
2022-02-16 10:53:09 -0800

    corrected dortenv inport to resolve conflict

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-require('dotenv');
+require('dotenv').config();
 const db = new Sequelize(process.env.DATABASE_URL || process.env.LOCAL_URL, {
   logging: false
 });
